88e42a86a35c2cc2e98f9123ab3760faf8865f8d
jinghua@nyquistdata.com <jinghua@nyquistdata.com>
2024-04-15 15:45:00 +0800

    feat: add update log file....

--- src/utils.ts
+++ src/utils.ts
@@ -10,4 +10,4 @@ export const CHAT_AWSSOME_JSON = 'chat.awesome.json';
 export const CHAT_NOTES_JSON = 'chat.notes.json';
 export const CHAT_PROMPTS_CSV = 'chat.prompts.csv';
 export const GITHUB_PROMPTS_CSV_URL = 'https://raw.githubusercontent.com/f/awesome-chatgpt-prompts/main/prompts.csv';
-export const GITHUB_LOG_URL = 'https://raw.githubusercontent.com/jinghualee2015/chat-app/main/UPDATE_LOG.md';
+export const GITHUB_LOG_URL = 'https://raw.githubusercontent.com/jinghualee2015/chat_app/main/UPDATE_LOG.md';
