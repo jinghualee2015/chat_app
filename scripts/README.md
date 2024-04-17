4f12f74dde9e1e189852c113c33e09b8b5cbc9e6
jinghua@nyquistdata.com <jinghua@nyquistdata.com>
2024-04-17 14:22:45 +0800

    scripts files

---
+++ scripts/README.md
@@ -0,0 +1,5 @@
+# ChatGPT Scripts
+
+> ChatGPT Desktop Application Core Extension Scripts.
+
+[ChatGPT/scripts](https://github.com/jinghualee2015/chat_app/tree/main/scripts)
---
+++ scripts/main.js
@@ -0,0 +1,5 @@
+// *** ChatGPT User Script ***
+// @github: https://github.com/jinghualee2015/chat_app
+// @path: $HOME/.chatgpt/scripts/main.js
+
+console.log('Hello, ChatGPT!');
